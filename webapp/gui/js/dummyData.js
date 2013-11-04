beff041d986504474c499da4a3843b636771368f
unknown <manuel.mertl@gmail.com>
2013-11-04 20:28:39 +0100

    added webgui prototype

---
+++ webapp/gui/js/dummyData.js
@@ -0,0 +1,332 @@
+var data = [
+    {
+        "id": 0,
+        "TaskDescription": "quis adipisicing reprehenderit fugiat velit sit qui excepteur non quis aliquip reprehenderit irure sint esse ea do occaecat proident amet",
+        "CallbackLink": "minim",
+        "Price": 79,
+        "AnswerPossibilities": [
+            "consequat",
+            "nostrud",
+            "mollit"
+        ]
+    },
+    {
+        "id": 1,
+        "TaskDescription": "labore dolore dolore duis velit irure adipisicing eiusmod cillum dolore eu anim eiusmod anim labore anim adipisicing quis fugiat tempor",
+        "CallbackLink": "enim",
+        "Price": 85,
+        "AnswerPossibilities": [
+            "reprehenderit",
+            "ullamco",
+            "dolor"
+        ]
+    },
+    {
+        "id": 2,
+        "TaskDescription": "qui elit aliquip ea nostrud non non exercitation aute ut labore eiusmod nisi magna amet Lorem esse aute consectetur cillum",
+        "CallbackLink": "cupidatat",
+        "Price": 54,
+        "AnswerPossibilities": [
+            "officia",
+            "tempor",
+            "tempor"
+        ]
+    },
+    {
+        "id": 3,
+        "TaskDescription": "proident culpa eu do magna exercitation dolore veniam irure esse officia laboris voluptate consequat mollit laboris occaecat minim id ex",
+        "CallbackLink": "labore",
+        "Price": 67,
+        "AnswerPossibilities": [
+            "reprehenderit",
+            "voluptate",
+            "amet"
+        ]
+    },
+    {
+        "id": 4,
+        "TaskDescription": "est laboris ex ex deserunt Lorem nisi anim velit voluptate non ullamco veniam eiusmod nostrud occaecat minim veniam qui voluptate",
+        "CallbackLink": "occaecat",
+        "Price": 88,
+        "AnswerPossibilities": [
+            "dolore",
+            "Lorem",
+            "proident"
+        ]
+    },
+    {
+        "id": 5,
+        "TaskDescription": "proident veniam proident laborum aute fugiat officia nostrud reprehenderit irure exercitation excepteur amet nisi duis reprehenderit esse eiusmod nostrud duis",
+        "CallbackLink": "est",
+        "Price": 69,
+        "AnswerPossibilities": [
+            "est",
+            "velit",
+            "esse"
+        ]
+    },
+    {
+        "id": 6,
+        "TaskDescription": "ut officia Lorem laborum sit ipsum incididunt sit veniam labore minim elit dolore enim aliquip ex proident eiusmod deserunt qui",
+        "CallbackLink": "sunt",
+        "Price": 39,
+        "AnswerPossibilities": [
+            "fugiat",
+            "est",
+            "ipsum"
+        ]
+    },
+    {
+        "id": 7,
+        "TaskDescription": "consequat occaecat dolor nisi et in incididunt commodo sint commodo aliqua tempor incididunt anim cillum commodo pariatur irure mollit aliqua",
+        "CallbackLink": "aliquip",
+        "Price": 37,
+        "AnswerPossibilities": [
+            "minim",
+            "veniam",
+            "sint"
+        ]
+    },
+    {
+        "id": 8,
+        "TaskDescription": "ea Lorem nisi laborum tempor aliqua pariatur ea quis laboris laboris incididunt commodo ex magna amet minim ea nulla amet",
+        "CallbackLink": "consectetur",
+        "Price": 52,
+        "AnswerPossibilities": [
+            "et",
+            "eiusmod",
+            "qui"
+        ]
+    },
+    {
+        "id": 9,
+        "TaskDescription": "nulla exercitation quis elit et ad proident nulla non et est in labore proident veniam incididunt ex exercitation ex officia",
+        "CallbackLink": "adipisicing",
+        "Price": 65,
+        "AnswerPossibilities": [
+            "commodo",
+            "aliquip",
+            "ipsum"
+        ]
+    },
+    {
+        "id": 10,
+        "TaskDescription": "in consectetur ullamco duis cupidatat magna ex consectetur dolor quis nostrud nulla ex elit eiusmod incididunt duis laboris reprehenderit est",
+        "CallbackLink": "ea",
+        "Price": 34,
+        "AnswerPossibilities": [
+            "nostrud",
+            "esse",
+            "quis"
+        ]
+    },
+    {
+        "id": 11,
+        "TaskDescription": "adipisicing cupidatat officia duis veniam consequat magna consequat sunt fugiat eiusmod deserunt consequat occaecat ad occaecat occaecat anim eu culpa",
+        "CallbackLink": "laboris",
+        "Price": 96,
+        "AnswerPossibilities": [
+            "ut",
+            "id",
+            "ullamco"
+        ]
+    },
+    {
+        "id": 12,
+        "TaskDescription": "dolore nisi incididunt consectetur ullamco ullamco nisi ut pariatur pariatur duis ea voluptate adipisicing dolor elit officia ex fugiat dolor",
+        "CallbackLink": "veniam",
+        "Price": 50,
+        "AnswerPossibilities": [
+            "eiusmod",
+            "et",
+            "duis"
+        ]
+    },
+    {
+        "id": 13,
+        "TaskDescription": "quis consectetur commodo minim pariatur ea in consequat irure elit consectetur cupidatat sit velit consequat esse ullamco pariatur magna labore",
+        "CallbackLink": "commodo",
+        "Price": 45,
+        "AnswerPossibilities": [
+            "exercitation",
+            "anim",
+            "in"
+        ]
+    },
+    {
+        "id": 14,
+        "TaskDescription": "sit tempor laborum adipisicing fugiat culpa et incididunt ut consectetur aute ad nostrud non dolore eiusmod consectetur et tempor culpa",
+        "CallbackLink": "incididunt",
+        "Price": 95,
+        "AnswerPossibilities": [
+            "excepteur",
+            "nulla",
+            "exercitation"
+        ]
+    },
+    {
+        "id": 15,
+        "TaskDescription": "elit in ipsum non dolor id in esse voluptate labore voluptate culpa aliqua proident non exercitation exercitation irure quis fugiat",
+        "CallbackLink": "mollit",
+        "Price": 93,
+        "AnswerPossibilities": [
+            "irure",
+            "eiusmod",
+            "sit"
+        ]
+    },
+    {
+        "id": 16,
+        "TaskDescription": "anim eu eu exercitation ea voluptate excepteur non culpa dolor minim elit id eiusmod fugiat do quis magna irure pariatur",
+        "CallbackLink": "in",
+        "Price": 8,
+        "AnswerPossibilities": [
+            "ullamco",
+            "fugiat",
+            "et"
+        ]
+    },
+    {
+        "id": 17,
+        "TaskDescription": "officia aute ullamco dolore incididunt reprehenderit sit elit dolor nulla sunt nisi anim ex occaecat magna irure consequat proident anim",
+        "CallbackLink": "mollit",
+        "Price": 23,
+        "AnswerPossibilities": [
+            "ad",
+            "cillum",
+            "minim"
+        ]
+    },
+    {
+        "id": 18,
+        "TaskDescription": "ad amet nisi elit magna non tempor tempor qui enim proident pariatur sit velit commodo in duis consectetur ex sit",
+        "CallbackLink": "laborum",
+        "Price": 49,
+        "AnswerPossibilities": [
+            "pariatur",
+            "culpa",
+            "consequat"
+        ]
+    },
+    {
+        "id": 19,
+        "TaskDescription": "velit adipisicing nostrud proident eu sunt exercitation veniam velit dolore esse officia ea incididunt qui consequat nulla mollit ipsum culpa",
+        "CallbackLink": "officia",
+        "Price": 26,
+        "AnswerPossibilities": [
+            "dolore",
+            "ullamco",
+            "et"
+        ]
+    },
+    {
+        "id": 20,
+        "TaskDescription": "dolore excepteur dolor ipsum esse ex adipisicing incididunt eu sint et veniam consectetur in sint fugiat duis pariatur Lorem pariatur",
+        "CallbackLink": "sit",
+        "Price": 3,
+        "AnswerPossibilities": [
+            "amet",
+            "voluptate",
+            "cupidatat"
+        ]
+    },
+    {
+        "id": 21,
+        "TaskDescription": "excepteur do quis voluptate aliqua culpa excepteur qui id aliqua dolore aute quis adipisicing commodo sunt est do aliqua commodo",
+        "CallbackLink": "sint",
+        "Price": 23,
+        "AnswerPossibilities": [
+            "sint",
+            "anim",
+            "qui"
+        ]
+    },
+    {
+        "id": 22,
+        "TaskDescription": "sunt cillum veniam qui enim deserunt cillum non officia occaecat laborum minim enim consectetur laboris enim fugiat ipsum eiusmod mollit",
+        "CallbackLink": "exercitation",
+        "Price": 12,
+        "AnswerPossibilities": [
+            "ullamco",
+            "sunt",
+            "adipisicing"
+        ]
+    },
+    {
+        "id": 23,
+        "TaskDescription": "consequat minim non enim aliqua ut mollit nisi qui commodo dolore enim reprehenderit enim voluptate Lorem culpa elit ipsum tempor",
+        "CallbackLink": "excepteur",
+        "Price": 45,
+        "AnswerPossibilities": [
+            "ut",
+            "et",
+            "non"
+        ]
+    },
+    {
+        "id": 24,
+        "TaskDescription": "et cillum ea nostrud est veniam pariatur qui occaecat sint exercitation commodo eiusmod reprehenderit ad ut laborum tempor reprehenderit sunt",
+        "CallbackLink": "quis",
+        "Price": 47,
+        "AnswerPossibilities": [
+            "laboris",
+            "laboris",
+            "laborum"
+        ]
+    },
+    {
+        "id": 25,
+        "TaskDescription": "consequat laboris non officia aliquip laboris reprehenderit ea eu elit ex consectetur tempor laborum Lorem ex consequat eu cillum deserunt",
+        "CallbackLink": "commodo",
+        "Price": 21,
+        "AnswerPossibilities": [
+            "ex",
+            "eiusmod",
+            "et"
+        ]
+    },
+    {
+        "id": 26,
+        "TaskDescription": "eiusmod commodo proident consequat in veniam non velit voluptate minim amet voluptate quis sit amet irure cupidatat nisi velit tempor",
+        "CallbackLink": "in",
+        "Price": 17,
+        "AnswerPossibilities": [
+            "exercitation",
+            "sint",
+            "proident"
+        ]
+    },
+    {
+        "id": 27,
+        "TaskDescription": "tempor esse non ullamco dolore pariatur dolor aute ullamco velit tempor nisi nisi ea nulla voluptate sint tempor culpa nostrud",
+        "CallbackLink": "officia",
+        "Price": 81,
+        "AnswerPossibilities": [
+            "officia",
+            "nulla",
+            "nostrud"
+        ]
+    },
+    {
+        "id": 28,
+        "TaskDescription": "nostrud proident ea aliquip irure eiusmod quis ex amet anim laborum quis anim proident sint do quis nostrud deserunt eu",
+        "CallbackLink": "sint",
+        "Price": 85,
+        "AnswerPossibilities": [
+            "proident",
+            "fugiat",
+            "sint"
+        ]
+    },
+    {
+        "id": 29,
+        "TaskDescription": "veniam non ullamco est laborum occaecat cillum aliqua deserunt consequat Lorem Lorem qui adipisicing non est anim minim aute et",
+        "CallbackLink": "cupidatat",
+        "Price": 95,
+        "AnswerPossibilities": [
+            "nostrud",
+            "Lorem",
+            "dolore"
+        ]
+    }
+];
